5044d41695ca360366b736d8dc6efce2b2434fdd
peeto <petersenc@gmail.com>
2016-06-19 17:35:02 +1000

    Find ridiculous speeds

--- examples.sql
+++ examples.sql
@@ -42,6 +42,22 @@ GROUP BY l.user, l.device, l.id
 ORDER BY l.prevdist DESC
 LIMIT 5;
 
+-- bad/flight data
+SELECT
+  l.id, l.user, l.device,
+  X(l.location) AS lat, Y(l.location) AS `long`,
+  CONCAT('https://www.google.com/maps/place/',X(l.location),',',Y(l.location)) AS url,
+  l.altitude, l.time, l.prevdist AS distance,
+  CONCAT('https://www.google.com/maps/place/',X(lp.location),',',Y(lp.location)) AS prevurl,
+  lp.time as prevtime, l.time - lp.time AS totaltime,
+  l.prevdist/(l.time - lp.time) AS speed_ms,
+  (l.prevdist/(l.time - lp.time))*3.6 AS speed_kmh
+FROM locations AS l
+LEFT JOIN locations lp ON lp.id=l.previd
+WHERE (l.prevdist/(l.time - lp.time))*3.6 > 200
+GROUP BY l.user, l.device, l.id
+ORDER BY (l.prevdist/(l.time - lp.time)) DESC;
+
 -- Monthly distance report
 SELECT 
   l.user, l.device, 
